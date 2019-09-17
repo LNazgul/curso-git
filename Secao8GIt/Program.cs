40ce70f43b8ea6e73d30855c99ee8ec4ff891a99
Louise Constantino <constantino.louise@gmail.com>
2019-09-17 20:16:28 -0300

    Acrescentada uma mensagem de Bom Dia

--- Secao8GIt/Program.cs
+++ Secao8GIt/Program.cs
@@ -4,6 +4,7 @@ namespace Secao8GIt {
     class Program {
         static void Main(string[] args) {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Bom dia!");
         }
     }
 }
