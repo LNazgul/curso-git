c4b856e1f2fa4a3cbff1619558fa195f943db10c
Louise Constantino <constantino.louise@gmail.com>
2019-09-17 20:35:54 -0300

    Acrescentada uma pergunta

--- Secao8GIt/Program.cs
+++ Secao8GIt/Program.cs
@@ -5,6 +5,7 @@ namespace Secao8GIt {
         static void Main(string[] args) {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Bom dia!");
+            Console.WriteLine("Como vc esta hoje?");
         }
     }
 }
