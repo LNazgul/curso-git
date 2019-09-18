e2b573528ea83897283b19af4e873c4db19c085e
Louise Constantino <constantino.louise@gmail.com>
2019-09-17 21:10:29 -0300

    Acrescentado tracos

--- Secao8GIt/Program.cs
+++ Secao8GIt/Program.cs
@@ -3,6 +3,7 @@
 namespace Secao8GIt {
     class Program {
         static void Main(string[] args) {
+            Console.WriteLine("-------------------------------");
             Console.WriteLine("Hello World!");            
             Console.WriteLine("Como vc esta hoje?");
             Console.WriteLine("Tenha um Bom dia!");
